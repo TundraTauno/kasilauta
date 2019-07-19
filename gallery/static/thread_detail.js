f8c5df5e28f2621e72201e565dfb5368fc9fdacb
TundraTauno <rekif@emailapps.in>
2019-07-20 01:12:50 +0300

    Linkify reply ids to refer local posts. Cross-board replies doesn't work yet.

--- gallery/static/thread_detail.js
+++ gallery/static/thread_detail.js
@@ -7,3 +7,44 @@ $(document).ready(function() {
       $(this).attr("src", current === orig ? thumb : orig);
     });    
 });
+
+// Linkify ids. This makes naive assumption that all ids are in the same thread.
+// This doesn't matter, since this is the case 99 % of the time and we can later
+// handle the "non-local" redirection.
+$(document).ready(function() {
+  $("p.post-text").each(function() {
+    link_prefix = '<a href=#';
+    link_postfix = '</a>';
+    var regex = /(^|\W)>>\d{1,11}/g;
+
+    matches = [];
+    while ((match = regex.exec($(this).text())) !== null) {
+      matches.push(match[0].trim());
+    }
+
+    var content = $(this).text();
+    matches.forEach(function(match) {
+      var replace_with = link_prefix + match.substr(2) + '>' + match + link_postfix;
+      content = content.replace(new RegExp(match, 'g'), replace_with);
+//       console.log('Replace ' + match + ' -> ' + replace_with);
+    });
+    $(this).html(content);
+  })
+});
+
+// Follow reply links.
+// TODO: Implement non-local redirection. 
+$(document).ready(function() {
+    var regex = /(^|\W)>>\d{1,11}/;
+    $("p.post-text a").click(function(event) {
+      var clicked = $(event.target).text().match(regex);
+      if (clicked) {
+        var number = clicked[0].substr(2);
+        if ($('#' + number).length == 0)  // not a local link TODO: implement redirection.
+          alert("Not a local link.");
+        else
+          $('#' + number).click();        // local link, go to post
+      }
+//       console.log(number[0].substr(2));
+    });    
+});
