88de5ae62bad8116a6ca4643c24b35e768df384c
TundraTauno <rekif@emailapps.in>
2019-10-02 23:02:13 +0300

    Make slug matcher for boards.

--- gallery/url_matchers.py
+++ gallery/url_matchers.py
@@ -1,7 +1,6 @@
-class board_url_matcher:
-    regex = '^[a-z]{1,20}$'
+class board_converter:
+    regex = '[a-z]{1,20}'
     def to_python(self, value):
         return value
-
     def to_url(self, value):
-        return str(value)
+        return value
--- gallery/urls.py
+++ gallery/urls.py
@@ -1,23 +1,26 @@
-from django.urls import path
-from . import views
+from django.urls import path, register_converter
+from . import url_matchers, views
 
 # For static files.
 from django.conf import settings
 from django.conf.urls.static import static
+
+register_converter(url_matchers.board_converter, 'board_slug')
 
 # Create your urls here.
 urlpatterns = [
         # index
         path('', views.index, name='index'),
         # board view 
-        path('<slug:name>/', views.board_view, name='board_view'),
+        path('<board_slug:name>/', views.board_view, name='board_view'),
         # thread detail 
-        path('<slug:name>/<int:id>', views.thread_detail, name='thread_detail'),
+        path('<board_slug:name>/<int:id>', views.thread_detail, name='thread_detail'),
+        # thread form
+        path('create_thread/<board_slug:board>', views.create_thread, name='create_thread'),
         # post form
-        path('create_thread/<slug:board>', views.create_thread, name='create_thread'),
-        path('create_post/<slug:board>/<int:thread>', views.create_post, name='create_post'),
+        path('create_post/<board_slug:board>/<int:thread>', views.create_post, name='create_post'),
         # user actions on post
-        path('user_action/<slug:board>/<int:thread>/<int:post>', views.user_action, name='user_action'),
+        path('user_action/<board_slug:board>/<int:thread>/<int:post>', views.user_action, name='user_action'),
         ]
 
 # Static media files for test environment. Not suitable for production.
